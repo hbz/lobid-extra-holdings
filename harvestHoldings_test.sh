2d8de6ab7b2108af52d720f0debd3912a614a4f7
TobiasNx <tobias.buelte@hbz-nrw.de>
2025-07-02 13:35:02 +0200

    Add test script

---
+++ harvestHoldings_test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+Metafacture_Runner=$1
+
+# Create Mapping file for SRU
+
+echo "Create Mapping for SRU" && date
+$Metafacture_Runner flux/createZdbId2AlmaMmsIdMap_test.flux
+
+# Create Holdings from old ALEPH Seq Dump
+echo "Start transformation for ALEPH Seq Data" && date
+$Metafacture_Runner flux/mab2De-Sol1Holdings_seq_test.flux outfile2="test/output/sol1Holding_seq.tsv"
+
+# Create Holdings from SRU Request
+echo "Start transformation for SRU Data" && date
+$Metafacture_Runner flux/zdbSru2De-Sol1Holdings_marc_test.flux outfile2="test/output/sol1Holding_sru.tsv"
+
+# Concatinate results
+echo "Combine output in single file combinedDe-Sol1Holdings.tsv" && date
+cat test/output/sol1Holding_sru.tsv test/output/sol1Holding_seq.tsv > "test/output/combinedDe-Sol1Holdings.tsv"
+echo "Done" && date
