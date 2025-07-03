56cfa6bd24a4500c432e1ae7c545a35547309bad
TobiasNx <tobias.buelte@hbz-nrw.de>
2025-07-03 14:14:49 +0200

    Rename files from batch to prod

--- harvestHoldings.sh
+++ harvestHoldings.sh
@@ -8,9 +8,9 @@ $Metafacture_Runner createZdbId2AlmaMmsIdMap_prod.flux
 
 # Create Holdings from old ALEPH Seq Dump
 
-$Metafacture_Runner mab2De-Sol1Holdings_seq_bulk.flux outfile2="prod/output/combinedDe-Sol1Holdings.tsv.gz"
+$Metafacture_Runner mab2De-Sol1Holdings_seq_prod.flux outfile2="prod/output/combinedDe-Sol1Holdings.tsv.gz"
 
 # Create Holdings from SRU Request
 
-$Metafacture_Runner zdbSRUMarcBibliographicAndHoldings2De-Sol1Holdings_bulk.flux outfile2="prod/output/combinedDe-Sol1Holdings.tsv.gz"
+$Metafacture_Runner zdbSRUMarcBibliographicAndHoldings2De-Sol1Holdings_prod.flux outfile2="prod/output/combinedDe-Sol1Holdings.tsv.gz"
 
